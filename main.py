3650ee3070b4c260c7ec587a7c0642de3a100779
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-12-24 13:58:11 +0100

    Added new functionality and fixes

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+import requests
+
+
+posts = requests.get("https://api.npoint.io/2614b7cf1520299f67ca").json()
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def get_all_posts():
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
 
 
 @app.route('/contact')
@@ -16,6 +20,15 @@ def contact_page():
 @app.route('/about')
 def about_page():
     return render_template('about.html')
+
+
+@app.route('/post/<int:index>')
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 
 if __name__ == "__main__":
