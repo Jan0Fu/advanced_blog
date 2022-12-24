77fa299209d1fca0711ce124b15f0e6ab6df2359
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-12-24 11:39:25 +0100

    Fixed bugs

--- main.py
+++ main.py
@@ -8,5 +8,15 @@ def get_all_posts():
     return render_template('index.html')
 
 
+@app.route('/contact')
+def contact_page():
+    return render_template('contact.html')
+
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
