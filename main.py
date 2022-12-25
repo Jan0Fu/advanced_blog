e0d867aa7451cb65b015decfeba99a43c9dd38b3
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-12-25 10:02:08 +0100

    Created new form feature

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import smtplib
 import requests
 
+email = ""
+password = ""
 
 posts = requests.get("https://api.npoint.io/2614b7cf1520299f67ca").json()
 
@@ -12,9 +15,22 @@ def get_all_posts():
     return render_template('index.html', posts=posts)
 
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact_page():
-    return render_template('contact.html')
+    if request.method == "POST":
+        data = request.form
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template('contact.html', msg_sent=True)
+    return render_template('contact.html', msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(email, password)
+        connection.sendmail(from_addr=email, to_addrs=email, email_message)
 
 
 @app.route('/about')
